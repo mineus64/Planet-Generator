5496d48f0ad91e09948e2eacc236129b9585f7fa
mineus64 <43474851+mineus64@users.noreply.github.com>
2022-02-01 03:44:40 -0800

    Initial Import
    - Created PlanetGenerator script for planet creation
    - Imported noise script to sample Perlin noise
    - Imported planet texture samples
    - Imported pre-generated planet textures

---
+++ PlanetGenerator.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetGenerator : MonoBehaviour
+{
+    #region Variables
+
+    #endregion
+    #region General Methods
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    #endregion
+    #region Specific Methods
+
+    #endregion
+}
